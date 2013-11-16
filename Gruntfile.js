5aebe07c8c34759bf658c6b71cd7468511ac0e0f
Jason Hamm <jhamm.business@gmail.com>
2013-11-16 11:37:23 -0500

    Add grunt open task

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,5 +99,5 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('server', ['less', 'configureProxies:server', 'connect:staticserver', 'connect:server', 'watch', 'open:dev']);
+  grunt.registerTask('server', ['less', 'configureProxies:server', 'connect:staticserver', 'connect:server', 'open:dev', 'watch']);
 };
